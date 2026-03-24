5a224793d560d8a4fcdaceedca4587f634467774
angel <108492882+angel-472@users.noreply.github.com>
2026-03-23 22:44:20 -0400

    Implement projects API and display project data on the Projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-  import { page } from '$app/stores';
+  export let data;
 
 </script>
-
 <main aria-label="Projects page" class="w-full flex flex-col gap-8 text-zinc-100">
   <section class="flex flex-col" aria-label="Projects Header">
     <h1 class="text-5xl sm:text-6xl font-bold">Projects</h1>
     <p class="text-lg text-zinc-400 mt-4">A collection of my recent work and side projects.</p>
   </section>
+
+  <pre>
+  Projects API Data:
+    
+  {JSON.stringify(data.projects, null, 2)}
+  </pre>
+
+  <!-- TODO: Render project cards and project page (like blog posts but styled differently) -->
 </main>
---
+++ src/routes/projects/+page.ts
@@ -0,0 +1,7 @@
+export async function load({ fetch }) {
+  const res = await fetch('/api/projects');
+  const projects = await res.json();
+  return {
+    projects
+  };
+}
