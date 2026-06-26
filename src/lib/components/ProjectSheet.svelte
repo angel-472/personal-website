84587abcbb0d73871c8a07c1c801af81264cd518
angel <108492882+angel-472@users.noreply.github.com>
2026-06-26 11:02:00 -0400

    Create placeholder project view sheet, work on show and hide state

--- src/lib/components/ProjectSheet.svelte
+++ src/lib/components/ProjectSheet.svelte
@@ -1,8 +1,74 @@
 <script>
   import { gsap } from "gsap";
+  import { onMount } from "svelte";
+
+  let sheetElement = $state();
+  let visible = $state(false);
+  let currentProject = $state();
+
+  // Listening to url changes SPA style
+  onMount(() => {
+    (() => {
+      const patchHistory = (type) => {
+        const original = history[type];
+        return function () {
+          const result = original.apply(this, arguments);
+          const event = new Event(type.toLowerCase());
+          event.arguments = arguments;
+          window.dispatchEvent(event);
+
+          const changeEvent = new Event("locationchange");
+          window.dispatchEvent(changeEvent);
+          return result;
+        };
+      };
+
+      // Override pushState and replaceState
+      history.pushState = patchHistory("pushState");
+      history.replaceState = patchHistory("replaceState");
+
+      // Listen to browser back/forward buttons
+      window.addEventListener("popstate", () => {
+        window.dispatchEvent(new Event("locationchange"));
+      });
+    })();
+
+    // Usage: Listen to your new global event
+    window.addEventListener("locationchange", () => {
+      console.log("URL changed to:", window.location.href);
+      console.log(getCurrentProject());
+    });
+  });
+
+  function getCurrentProject() {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const projectSlug = urlParams.get("project");
+
+    return projectSlug;
+  }
+
+  $effect(() => {
+    if(currentProject !== undefined && currentProject !== ""){
+      show();
+    } 
+    else {
+      hide();
+    }
+  });
+  function show() {
+    console.log('showing')
+  }
+  function hide(){
+    console.log('hiding')
+  }
 </script>
 
-
-<div class="fixed bottom-0 left-1/2 -translate-x-1/2 h-[90vh] bg-zinc-50 flex">
-  <p>Project overlay sheet thing</p>
-</div>
+{#if visible}
+  <div
+    class="fixed bottom-0 left-1/2 -translate-x-1/2 h-[90vh] bg-zinc-50 flex"
+    bind:this={sheetElement}
+  >
+    <p>Project overlay sheet thing</p>
+  </div>
+{/if}
